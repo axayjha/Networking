63992b0d66fb1f41fe2cce56434603bc01f829fc
axayjha <akshayjha@live.in>
2018-05-11 22:42:06 +0530

    cleaned up a bit

--- CalcServer.java
+++ CalcServer.java
@@ -4,32 +4,36 @@ public class CalcServer
 {
 	public static void main(String args[]) throws IOException
 	{
+		/* creating sockets */
 		ServerSocket servsoc = new ServerSocket(1313);
 		Socket clientSocket = servsoc.accept();
 
+		/* getting input from client */
 		BufferedReader inFromClient = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));		
-		String numbers=inFromClient.readLine();
+		String input = inFromClient.readLine();
 
-		
-		numbers = numbers + " ";
-		String arr[]= numbers.split(" ");
+		/* parsing input string */
+		input = input + " ";
+		String arr[]= input.split(" ");
 		System.out.println(arr[0] + arr[1] + arr[2]);
+		int result = 0, n1 = Integer.parseInt(arr[0]), n2 = Integer.parseInt(arr[1]);
 
-		int ans = 0, n1 = Integer.parseInt(arr[0]), n2 = Integer.parseInt(arr[1]);
+		/* doing requires calculations */
 		if(arr[2].equals("+"))	
-			ans = n1 + n2;
+			result = n1 + n2;
 		
 		else if(arr[2].equals("-"))		
-			ans = n1 - n2;
+			result = n1 - n2;
 		
 		else if(arr[2].equals("*"))		
-			ans = n1 * n2;
+			result = n1 * n2;
 		
 		else		
-			ans = n1 / n2;		
+			result = n1 / n2;		
 		
+		/* sending back the result to client */
 		DataOutputStream outToClient = new DataOutputStream(clientSocket.getOutputStream());
-		outToClient.writeBytes(Integer.toString(ans) + "\n");
+		outToClient.writeBytes(Integer.toString(result) + "\n");
 		clientSocket.close();
 	}
 }
