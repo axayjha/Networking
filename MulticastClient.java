858be59825a87c599865132b1ce6759c03db2188
axayjha <akshayjha@live.in>
2018-05-12 07:45:44 +0530

    shortened code

--- MulticastClient.java
+++ MulticastClient.java
@@ -6,21 +6,18 @@ import java.util.Date;
 import java.io.*;
 import java.net.*;
 
-public class MulticastClient{
-	public static void main(String []args) throws IOException{
+public class MulticastClient {
+	public static void main(String[] args) throws IOException {
 		MulticastSocket socket = new MulticastSocket(1315);
 		InetAddress group = InetAddress.getByName("230.0.0.3");
 		socket.joinGroup(group);
-		for(int i=0; i<10; i++)
-		{
-			byte[] buf= new byte[256];
-			DatagramPacket packet = new DatagramPacket(buf, buf.length);
-			socket.receive(packet);
-			String received = new String(packet.getData());
-			System.out.println("Current server time: " + received); 
-		}
+
+		byte[]buf = new byte[256];
+		DatagramPacket packet = new DatagramPacket(buf, buf.length);
+		socket.receive(packet);
+		System.out.println("Current server time: " + new String(packet.getData()));
+
 		socket.leaveGroup(group);
 		socket.close();
-
 	}
 }
